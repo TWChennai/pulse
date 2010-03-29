428f04efaf16ad9b3ee09fb00f70e3c4629c1652
Arvind Kunday <kunday@gmail.com>
2010-03-29 16:12:49 +0530

    Project Controller now can save some information into the database, not complete yet

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -15,7 +15,11 @@ class ProjectController < ApplicationController
     db = CouchRest.database!("http://127.0.0.1:5984/projects")
     @project_template = db.get("56071a715dc0d9b24c5804f966f179e6")
     @tracked_properties = @project_template["tracking"]["properties"]
-    puts params[:properties].to_json
+    @json_to_save=construct_date_json(@tracked_properties,params[:properties])
+    @response_from_server = db.save_doc({:properties=>@json_to_save})
+    @id = @response_from_server["id"]
+    redirect_to(:action=>'show',:id=>@id)
+
   end
   private
   def construct_date_json(tracked_properties, actual_properties )
@@ -27,6 +31,6 @@ class ProjectController < ApplicationController
     return actual_properties
   end
   def get_date_from_hash(date_hash)
-    Date.civil(date_hash["date(1i)"].to_i,date_hash["date(2i)"].to_i,date_hash["date(3i)"].to_i).to_json
+    Date.civil(date_hash["date(1i)"].to_i,date_hash["date(2i)"].to_i,date_hash["date(3i)"].to_i)
   end
 end
