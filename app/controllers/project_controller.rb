0735698e129b756586a9393022dafdbd41c5632b
Arvind Kunday <kunday@gmail.com>
2010-03-29 14:05:45 +0530

    Changes to the create project page

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -12,6 +12,21 @@ class ProjectController < ApplicationController
     @fields = @project_template["tracking"]["properties"].sort{|a,b| a[1]["order"]<=>b[1]["order"]}
   end
   def save
+    db = CouchRest.database!("http://127.0.0.1:5984/projects")
+    @project_template = db.get("56071a715dc0d9b24c5804f966f179e6")
+    @tracked_properties = @project_template["tracking"]["properties"]
     puts params[:properties].to_json
+  end
+  private
+  def construct_date_json(tracked_properties, actual_properties )
+    tracked_properties.each do |property|
+      if property[1]["type"] == "date"
+        actual_properties[property[0]]= get_date_from_hash(actual_properties[property[0]])
+      end
+    end
+    return actual_properties
+  end
+  def get_date_from_hash(date_hash)
+    Date.civil(date_hash["date(1i)"].to_i,date_hash["date(2i)"].to_i,date_hash["date(3i)"].to_i).to_json
   end
 end
