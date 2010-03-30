b32c254fbc7edb3e24376662b9076444e965912c
Arvind Kunday <kunday@gmail.com>
2010-03-30 23:23:53 +0530

    Refactored the views and Removed some amount of redundancy \n 1. Also adding layout css's to show the create page in a presentable form

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -5,4 +5,20 @@ module ProjectHelper
     end
       return Date.today
   end
+  def generate_check_box_tag(metric_name, mandatory="yes")
+    if mandatory == "yes"
+      check_box_tag "metrics[#{metric_name}]", "yes",true, :disabled =>true
+    else
+      check_box_tag "metrics[#{metric_name}]", "yes"
+    end
+  end
+  def generate_form_tag_for_property(field_hash)
+    if field_hash[1]["type"] == "date"
+      date = convert_to_date(field_hash[1]["value"])
+      date_select("properties[#{field_hash[0]}]", "date", :start_year => 1995,:order => [:day, :month, :year],
+                  :default=>{:day => date.day, :year=>date.year, :month=>date.month})
+    else
+      text_field_tag "properties[#{field_hash[0]}]", field_hash[1]['value']
+    end
+  end
 end
