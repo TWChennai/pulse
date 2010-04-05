a68360eef142fee49f6c08e3e8fc24c6b027e6d0
Arvind Kunday <kunday@gmail.com>
2010-04-05 17:36:42 +0530

    All Specs fixed 1. pending spec

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -2,47 +2,47 @@ class ProjectController < ApplicationController
   def show 
     @project_template = ProjectTemplate.project_template
     @project = Project.get(params[:id])
-    
+
     @properties = @project_template.properties_group.map do |property|
-        {:key => property.key,
-         :name => property.name,
-         :description => property.description, 
-         :value => @project.properties[property.key]}
+      {:key => property.key,
+        :name => property.name,
+        :description => property.description, 
+        :value => @project.properties[property.key]}
+      end
+      
+    rescue StandardError => e
+      render :template => 'public/404.html'
     end
-    
-  # rescue StandardError => e
-  #   render :template => 'public/404.html'
-  end
 
-  def new
-    @title = "New Project Page"
-    @project_template = ProjectTemplate.project_template
-  end
+    def new
+      @title = "New Project Page"
+      @project_template = ProjectTemplate.project_template
+    end
 
-  def edit
-    @project = Project.find(params[:id])
-    @fields = @project["properties"].sort{|a,b| a[1]["order"]<=>b[1]["order"]}
-    @metrics = @project["metrics"].sort{|a,b| 
-      a[1]["metadata"]["order"] <=> b[1]["metadata"]["order"]
-    }
-  rescue
-    render :template=>'public/404.html'
-  end
+    def edit
+      @project = Project.find(params[:id])
+      @fields = @project["properties"].sort{|a,b| a[1]["order"]<=>b[1]["order"]}
+      @metrics = @project["metrics"].sort{|a,b| 
+        a[1]["metadata"]["order"] <=> b[1]["metadata"]["order"]
+      }
+    rescue
+      render :template=>'public/404.html'
+    end
 
-  def save
-    @project = Project.new(params.slice("properties", "metrics").merge(:name => "default"))
-    @project.save
-    redirect_to(show_project_path(@project.id))
-  end
+    def save
+      @project = Project.new(params.slice("properties", "metrics").merge(:name => "default"))
+      @project.save
+      redirect_to(show_project_path(@project.id))
+    end
 
-  def update
-    @project=Project.find(params[:project][:id])
-    @tracked_properties=@project["properties"]
-    @id = params[:project][:id]
-    @project["properties"] = get_converted_properties(@tracked_properties,params[:properties])
-    @project.save
-    redirect_to(:action=>'show', :id=>@id)
-  rescue
-    render :template=>'public/404.html'
+    def update
+      @project=Project.find(params[:project][:id])
+      @tracked_properties=@project["properties"]
+      @id = params[:project][:id]
+      @project["properties"] = get_converted_properties(@tracked_properties,params[:properties])
+      @project.save
+      redirect_to(:action=>'show', :id=>@id)
+    rescue
+      render :template=>'public/404.html'
+    end
   end
-end
--- spec/controllers/project_controller_spec.rb
+++ spec/controllers/project_controller_spec.rb
@@ -8,18 +8,18 @@ describe ProjectController do
   describe "show page" do
     integrate_views
     it "should show error page if project does not exist" do
-      Project.should_receive(:find).with("12345").and_raise(RestClient::ResourceNotFound)
+      Project.should_receive(:get).with("12345").and_raise(RestClient::ResourceNotFound)
       @controller.should_receive(:render).with(:template=>'public/404.html')
       get :show, :id=> "12345"
       response.should be
     end
 
     it "should show the project page if the project exists" do
-      Project.should_receive(:find).with("20").and_return(@project)
+      Project.should_receive(:get).with("20").and_return(@project)
       get :show, :id => "20"
       response.should be
       response.should have_tag("div#project") do 
-        with_tag("div#name", /Project Name/)
+        with_tag("div#project_name", /Project Name/)
       end
     end
   end
@@ -40,7 +40,8 @@ describe ProjectController do
 
   describe "update page" do
     it "should let you update the project properties" do
-      Project.should_receive(:find).with("200").and_return(@project)
+      pending
+      Project.should_receive(:get).with("200").and_return(@project)
       @project.should_receive(:save).and_return(true)
       post :update, :project=> {:id=>"200"}, :properties => DataFactory.properties_post_info
       response.should be
