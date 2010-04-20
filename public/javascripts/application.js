51c18056965875fa8d6b53ce5f5d76caddb2b211
Selva <k.n.selvakumar@gmail.com>
2010-04-20 15:55:45 +0530

    Selva : Made project properties toggable

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,28 +1,33 @@
 $(document).ready(function(){
-	$('.iteration').dataTable();
-	$(".datepicker").datepicker();	
-	$(".indicator").tooltip();
+  $('.iteration').dataTable();
+  $(".datepicker").datepicker();	
+  $(".indicator").tooltip();
+
+  $("#project_properties_title").click(function(){
+    $('#project_properties').toggle(400);
+    return false;
+  });
 });
 
 var Attachment = {
-	attachments_count : 0,
-	remove_link : function(){
-		return $('<a />').attr('class', 'remove_attachment').attr('href', '#').text('Remove Attachment');
-	},
-	
-	list_item : function(){
-		return $('<li />').html("<input type='file' name='attachments[]' />").append(Attachment.remove_link());
-	},
-	
-	init : function(){
-		$("#attachments a.add").click(function(){
-			$("#attachments ul").append(Attachment.list_item());
-			Attachment.attachments_count++;
-			return false;
-		});
-		$(".remove_attachment").live('click', function(){
-			$(this).parent().remove();
-			return false;
-		});
-	}
+  attachments_count : 0,
+  remove_link : function(){
+    return $('<a />').attr('class', 'remove_attachment').attr('href', '#').text('Remove Attachment');
+  },
+
+  list_item : function(){
+    return $('<li />').html("<input type='file' name='attachments[]' />").append(Attachment.remove_link());
+  },
+
+  init : function(){
+    $("#attachments a.add").click(function(){
+      $("#attachments ul").append(Attachment.list_item());
+      Attachment.attachments_count++;
+      return false;
+    });
+    $(".remove_attachment").live('click', function(){
+      $(this).parent().remove();
+      return false;
+    });
+  }
 };
