dc85cb67ff61d9967bbece644ddbca0c85d470ef
Arvind Kunday <arvinda@thoughtworks.com>
2010-05-10 11:31:50 +0530

    Application js modified

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('.iteration').dataTable({"bAutoWidth": false});
+  $('.iteration').dataTable({"bAutoWidth": false});	
   $(".datepicker").datepicker();	
   $(".indicator").tooltip();
 
