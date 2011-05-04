164bf9e478a2bc548dd5a85bf62346fc8d11bc99
Rekha <rekhas@thoughtworks.com>
2011-05-04 17:43:44 +0530

    Rekha - changing 3-color indicators to 5-color

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,10 +1,12 @@
 class Metric < CouchRest::ExtendedDocument
   module Value
     RED = "Red"
+    LIGHTGREEN = "Lightgreen"
+    ORANGE = "Orange"
     GREEN = "Green"
     AMBER = "Amber"
   end
-  VALUES = [Metric::Value::RED, Metric::Value::GREEN, Metric::Value::AMBER]
+  VALUES = [Metric::Value::RED, Metric::Value::LIGHTGREEN, Metric::Value::ORANGE, Metric::Value::GREEN, Metric::Value::AMBER]
   
   property :name
   property :value
